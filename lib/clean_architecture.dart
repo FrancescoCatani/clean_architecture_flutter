e48775d8a5c2905e272a53df4ed23608d33db482
Francesco Catani <francescocatanipersonal@gmail.com>
2020-07-29 15:39:56 +0200

    1.0.0

--- lib/clean_architecture.dart
+++ lib/clean_architecture.dart
@@ -1,8 +1,17 @@
-library clean_architecture;
+import 'package:flutter/material.dart';
 
-import 'package:flutter_core/flutter_core.dart';
+void main() => runApp(MyApp());
 
-
-class Main {
-
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: null,
+    );
+  }
 }
